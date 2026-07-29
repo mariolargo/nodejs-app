f68ca2a18f0c3b96b4acc3734ae2106135aee3c7
Mario Largo <mlargo@yahoo.com>
2026-07-29 15:06:53 -0500

    feat: Node.JS with Redis

---
+++ backend/src/index.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import responseTime from 'response-time'
+import { createClient } from 'redis'
+
+const client = await createClient({
+  host: '127.0.0.1',
+  port: 6379,
+}).connect()
+
+client.on('error', (error) => {
+  console.error(error)
+})
+
+const app = express()
+
+app.use(responseTime())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/character', async (req, res) => {
+  try {
+    const cache = await client.get('characters')
+    if (cache) {
+      return res.json(JSON.parse(cache))
+    }
+    // https://jsonplaceholder.typicode.com/posts
+    // https://rickandmortyapi.com/api/character
+    const response = await fetch('https://rickandmortyapi.com/api/character', { signal: AbortSignal.timeout(5000) })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.json()
+    await client.set('characters', JSON.stringify(data))
+    res.json(data)
+  } catch (error) {
+    console.error('Fetch error:', error)
+  }
+})
+
+app.get('/character/:id', async (req, res) => {
+  try {
+    const cache = await client.get('character_' + req.params.id)
+    if (cache) {
+      return res.json(JSON.parse(cache))
+    }
+    const response = await fetch('https://rickandmortyapi.com/api/character/' + req.params.id, {
+      signal: AbortSignal.timeout(5000),
+    })
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = await response.json()
+    await client.set('character_' + req.params.id, JSON.stringify(data))
+    res.json(data)
+  } catch (error) {
+    console.error('Fetch error:', error)
+  }
+})
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
