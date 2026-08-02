9201995e11a1c9319a8169f2eadfe8cf367eff6d
Mario Largo <mlargo@yahoo.com>
2026-08-02 10:33:10 -0500

    feat: Add github actions

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,3 +1,5 @@
 export const testEnvironment = 'node'
 export const verbose = true
 export const clearMocks = true
+export const collectCoverage = true
+export const coverageReporters = ['json', 'lcov', 'text', 'clover']
